b653357e6a2df4735cdfd6473d6e999983b11696
Phil Wang <lucidrains@gmail.com>
2022-03-24 09:28:14 -0700

    complete basic autoregressive transformer

--- memorizing_transformers_pytorch/memorizing_transformers_pytorch.py
+++ memorizing_transformers_pytorch/memorizing_transformers_pytorch.py
@@ -1,3 +1,6 @@
+import math
+from functools import partial
+
 import torch
 import torch.nn.functional as F
 from torch import nn, einsum
@@ -11,6 +14,9 @@ def exists(val):
 def default(val, d):
     return val if exists(val) else d
 
+def l2norm(t):
+    return F.normalize(t, dim = -1)
+
 # helper classes
 
 class PreNormResidual(nn.Module):
@@ -21,6 +27,50 @@ class PreNormResidual(nn.Module):
 
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs) + x
+
+# t5 relative positional bias
+
+class T5RelativePositionBias(nn.Module):
+    def __init__(
+        self,
+        scale,
+        num_buckets = 32,
+        max_distance = 128,
+        heads = 8
+    ):
+        super().__init__()
+        self.scale = scale
+        self.num_buckets = num_buckets
+        self.max_distance = max_distance
+        self.relative_attention_bias = nn.Embedding(num_buckets, heads)
+
+    @staticmethod
+    def _relative_position_bucket(
+        relative_position,
+        num_buckets = 32,
+        max_distance = 128
+    ):
+        n = -relative_position
+        n = torch.max(n, torch.zeros_like(n))
+
+        max_exact = num_buckets // 2
+        is_small = n < max_exact
+
+        val_if_large = max_exact + (torch.log(n.float() / max_exact) / math.log(max_distance / max_exact) * (num_buckets - max_exact)).long()
+        val_if_large = torch.min(val_if_large, torch.full_like(val_if_large, num_buckets - 1))
+        return torch.where(is_small, n, val_if_large)
+
+    def forward(self, qk_dots):
+        i, j, device = *qk_dots.shape[-2:], qk_dots.device
+        q_pos = torch.arange(i, dtype = torch.long, device = device)
+        k_pos = torch.arange(j, dtype = torch.long, device = device)
+        rel_pos = rearrange(k_pos, 'j -> 1 j') - rearrange(q_pos, 'i -> i 1')
+        rp_bucket = self._relative_position_bucket(rel_pos, num_buckets = self.num_buckets, max_distance = self.max_distance)
+        values = self.relative_attention_bias(rp_bucket)
+        bias = rearrange(values, 'i j h -> () h i j')
+        return bias * self.scale
+
+# feedforward
 
 class FeedForward(nn.Module):
     def __init__(self, dim, mult = 4, dropout = 0.):
@@ -35,11 +85,89 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+# attention
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads = 8,
+        dim_head = 64,
+        dropout = 0.
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = heads * dim_head
+
+        self.rel_pos_bias = T5RelativePositionBias(scale = dim_head ** 0.5)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+
+    def forward(self, x, **kwargs):
+        h = self.heads
+        q, k, v = (self.to_q(x), *self.to_kv(x).chunk(2, dim = -1))
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+        q = q * self.scale
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        sim = self.rel_pos_bias(sim) + sim
+
+        i, j = sim.shape[-2:]
+        causal_mask = torch.ones((i, j), dtype = torch.bool).triu(j - i + 1)
+        sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        sim = sim - sim.amax(dim = -1, keepdim = True).detach()
+        attn = sim.softmax(dim = -1)
+        attn = self.dropout(attn)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
 # main class
 
 class MemorizingTransformer(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        *,
+        num_tokens,
+        dim,
+        depth,
+        dim_head = 64,
+        heads = 8,
+        attn_dropout = 0.,
+        ff_mult = 4,
+        ff_dropout = 0.
+    ):
         super().__init__()
+        self.token_emb = nn.Embedding(num_tokens, dim)
+
+        block_wrapper = partial(PreNormResidual, dim)
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                block_wrapper(Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = attn_dropout)),
+                block_wrapper(FeedForward(dim = dim, mult = ff_mult, dropout = ff_dropout)),
+            ]))
+
+        self.to_logits = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_tokens)
+        )
 
     def forward(self, x):
-        return x
+        x = self.token_emb(x)
+
+        for attn, ff in self.layers:
+            x = attn(x)
+            x = ff(x)
+
+        return self.to_logits(x)
