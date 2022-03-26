4428125e018f014566810e45df27f9e1077bd4c7
Phil Wang <lucidrains@gmail.com>
2022-03-26 15:13:35 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'memorizing-transformers-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.18',
+  version = '0.0.19',
   license='MIT',
   description = 'Memorizing Transformer - Pytorch',
   author = 'Phil Wang',
